91df41aad26e0e0db285cd698ec74926e393767c
cht <cht527@126.com>
2017-06-09 17:41:08 +0800

    add fly method

--- js/zoneIndex.js
+++ js/zoneIndex.js
@@ -183,36 +183,50 @@ var showNewText=function(){
 showNewText();
 
 //-------photo img click-------
-/* $(".photoimg").on('click',function(){
- 	var id=$(this).attr('id');
- 	var targetId=id.split('_')[1];
- 	//bgmPlay(targetId)
- 	var Id='myaudio_'+musicMark;
-	var oAudio = document.getElementById(Id); 
-	if(oAudio.paused){
-	    oAudio.play();
-	}else{
-	    oAudio.pause();
-	}
- })*/
-/* function bgmPlay(musicMark){
- 	var music_file="include/music/"+musicMark+".mp3";
- 	if (window.HTMLAudioElement) {
-		try {
-			var targetId='myaudio_'+musicMark;
-			var oAudio = document.getElementById(targetId); 
-			oAudio.src = music_file;
-			if(oAudio.paused){
-	            oAudio.play();
-	        }else{
-	            oAudio.pause();
-	        }
-		}catch (e) {
-						               
-			if(window.console && console.error("Error:" + e));
-		}
-	}  
- }*/
+
+var e=["生命中，有些人来了又去，有些人去而复返，有些人近在咫尺，有些人远在天涯，有些人擦身而过，有些人一路同行。\n无论如何，终免不了曲终人散的伤感。远在天涯的朋友：或许已是遥远得无法问候，但还是谢谢您曾经的结伴同行。","前端路上，一路有你。编辑你的正能量，快乐工作，努力生活~","此刻，我认真的微笑了，不带酸涩，不带惆怅，只是腼腆、淡然的微笑，那些刻有我成长符号的隐隐岁月，在不知不觉中幻化点点的自信，铺洒在我前进的道路上绽放星芒。","有些时候，简简单单也是一种幸福。的确，安于平淡安详，也是一种修养、一种享受。\n只是有些时候，平淡而不厌烦的生活已经很难在我们身上出现了。现实的状况往往让我们觉得物是人非，于是便有了烦恼，有了抱怨。","你的生命必须是留有缝隙，那样阳光才是能照进来。\n人生的最大的魅力不是成功，而是责任；生命惟因其短，故是应把它化入人类最壮丽的文明史中以获得永恒；生命也是唯因其短，更是要加倍珍惜每刻青春。","【人生感悟】有的时候，真的觉得人生最大的遗憾就是，放弃了不该放弃的，而固执地去坚持不该坚持的。\n生活有苦有甜，才叫完整；爱情有闹有和，才叫情趣；心情有悲有喜，才叫体会；日子有阴有晴，才叫自然；联系时有时无，才叫珍贵。","不要总在过去的回忆里缠绵，昨天的太阳，晒不干今天的衣裳。没有人可以赢得世界，但是大部分人都是输给了自己。最厌烦的感觉不是成为陌生人、而是逐渐陌生的态度。","人生的路只有经历过，才知道有短有长；时光如水，我们无法阻止岁月的脚步，很多人，逐渐淡出了我们的视线，懂得了什么叫过客，很多事，已经不再属于我们的精彩，知道了这就叫过往。","【人生感悟】人生是一程单向旅行，不可后退。走到生命的哪一个阶段，都该喜欢那一段时光，完成那一阶段该完成的职责，顺生而行，不沉迷过去，不狂热地期待着未来，生命理当如此。","有人说：“我若是灯，我就要用我的光明来照彻黑暗。”我不配做一盏明灯。那么就让我做一块木柴罢。我愿意把我从太阳那里受到的热放散出来，我愿意把自己烧得粉身碎骨给人间添一点点温暖。"];
+$(".js_fly").click(function() {
+    return $(".js_fly_message").val(e[Math.floor(10 * Math.random())]),
+    $(".js_fly_wind, .js_fly_plane").fadeIn(200),
+    !1
+})
+$(".js_fly_close").click(function() {
+    $(".js_fly_wind").fadeOut(200)
+})
+$(".js_fly_comment").click(function() {
+    $(this).toggleClass("choose_active")
+})
+$(".js_fly_send").click(function() {
+    //var e = "1288460501986050384";
+    if ($(".js_fly_comment").hasClass("choose_active")) {
+    	setTimeout(function() {
+        	$(".js_fly_plane").removeClass("front"),
+        	$(".js_fly_container").removeClass("beginning"),
+        	$(".js_fly_curvable").addClass("curved"),
+	        setTimeout(function() {
+	            $(".js_fly_container").addClass("hover"),
+	            setTimeout(function() {
+	                $(".js_fly_container").addClass("fly_away_first"),
+	                setTimeout(function() {
+	                    $(".js_fly_container").addClass("fly_away"),
+	                    setTimeout(function() {
+	                        $(".js_fly_plane").addClass("front"),
+	                        $(".js_fly_container").removeClass("fly_away fly_away_first hover").addClass("beginning"),
+	                        $(".js_fly_curvable").removeClass("curved"),
+	                        $(".js_fly_wind").hide()
+	                    },
+	                    3e3)
+	                },
+	                600)
+	            },
+	            2e3)
+	        },
+	        2800)
+    	},
+    	200)
+    }
+})
+
 
  //---监听展开pianoList 展开、收起
  $("#piano_items_contianer").on("click",'.show-hide-piano',function(){
