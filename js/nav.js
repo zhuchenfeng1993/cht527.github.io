736410729c5585aa79c40807e4c14f908a10558b
cht <cht527@126.com>
2017-06-11 10:40:22 +0800

    fix logical error of Pinyin

--- js/nav.js
+++ js/nav.js
@@ -17,7 +17,7 @@ var	diaryLogin_obj={
 		    var reg = new RegExp('[a-zA-Z0-9\- ]');
 		    for (var i=0;i<l2;i++) {
 		        var val = l1.substr(i,1);
-		        var name = this.arraySearch(val,PinYin);
+		        var name = this.arraySearch(val,this.PinYin);
 		        if(reg.test(val)) {
 		            I1 += val;
 		        } else if (name!==false) {
