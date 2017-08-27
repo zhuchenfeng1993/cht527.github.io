5b2bf6f8dfa951f928e86dd377e0cb2db98f9f90
cht527 <cht527@126.com>
2017-08-27 22:37:50 +0800

    finish add & delete function

--- potato/potato.js
+++ potato/potato.js
@@ -13,35 +13,49 @@ function Todo(id,text) {
 }
 
 var chtPotato={
-	init:function () {
-		this.todos = store(ADD_KEY);
-		this.findDom();
-		this.bindEvent();
-		this.render(); // 初始化渲染
-	},
-	findDom:function(argument){
-        this.todoList = document.querySelector("#todoList");//列表
-        this.todoListItem = document.querySelector(".todoItem");//列表项
-        this.addTask=document.querySelector("#addTask");//添加任务
-        this.delTask=document.querySelector(".delTask");//删除任务
-        this.startTask=document.querySelector("#startTask");//开始任务
-        this.pauseTask=document.querySelector("#pauseTask");// 暂停任务
+  	init:function () {
+  		this.todos = store(APP_KEY);
+  		this.findDom();
+  		this.bindEvent();
+  		this.render(); // 初始化渲染
+  	},
+  	findDom:function(argument){
+          this.todoList = document.querySelector("#todoList");//列表
+          this.todoListItem = document.querySelector(".todoItem");//列表项
+          this.addTask=document.querySelector("#addTask");//添加任务
+          this.startTask=document.querySelector("#startTask");//开始任务
+          this.pauseTask=document.querySelector("#pauseTask");// 暂停任务
+          this.innerBoxTask=document.querySelector("#innerBoxTask");// 任务名称
+          this.innerBoxTimer=document.querySelector("#innerBoxTimer");// 倒计时时钟
+
     },
      // 模拟 Controller (业务逻辑层)
     bindEvent:function(argument){
+        //---添加 新任务
         this.addTask.addEventListener('click', function (){
             this.addItem();
-        }, false);
- 
-        this.delTask.addEventListener('click', function(e){  // 事件委托，优化性能
-           this.delItem(item);
-        }, false);
+        }.bind(this), false);
+        //---删除 任务----
+        $("#todoList").on('click','.del-content',function(e){  // 事件委托，优化性能
+           e.preventDefault();
+           var targetIndex=e.target.getAttribute('data-index');
+           var targetContent=e.target.getAttribute('data-content');
+           this.delItem(targetIndex,targetContent);
+        }.bind(this));
+         //---添加当前任务----
+        $("#todoList").on('click','.item-content',function(e){  // 事件委托，优化性能
+           e.preventDefault();
+           var targetContent=e.target.innerHTML;
+           this.innerBoxTask.innerHTML="在"+this.timeFormater()+"完成"+targetContent+"，加油^_^";
+        }.bind(this));
+        //---开始 任务----
         this.startTask.addEventListener('click', function (){
             this.start();
-        }, false);
+        }.bind(this), false);
+        //---暂停 任务----
         this.pauseTask.addEventListener('click', function (){
             this.pause();
-        }, false);
+        }.bind(this), false);
     },
     // 这里勉强抽象成一个视图吧!!!
     view:function() {
@@ -49,9 +63,22 @@ var chtPotato={
           fragment = '';
   
           for (var i = 0; i < this.todos.length; i++) { // 一次性DOM节点生成
-            fragment += '<li class="todoItem">'+this.todos[i].text+'</li>';
+            fragment += '<li class="todoItem"><span class="item-icon"></span><span class="item-content">'+
+                        this.todos[i].text+'</span><span data-content='+this.todos[i].text+' data-index='+i+' class="del-content">×</span></li>';
+      
           }
           this.todoList.innerHTML = fragment;
+    },
+    timeFormater:function () {
+      var startTime=new Date();
+      var start_hh=startTime.getHours(),start_mm=startTime.getMinutes(); 
+      start_hh=start_hh>=10?start_hh:"0"+start_hh;  
+      start_mm=start_mm>=10?start_mm:"0"+start_mm;  
+      var dur=25*60*1000;//25分钟
+      var endTime=new Date(startTime.getTime()+dur),end_hh=endTime.getHours(),end_mm=endTime.getMinutes();                
+      end_hh=end_hh>=10?end_hh:"0"+end_hh;  
+      end_mm=end_mm>=10?end_mm:"0"+end_mm;
+      return start_hh+":"+start_mm+" - "+end_hh+":"+end_mm;
     },
   
         // render()函数作为一个订阅者的回调函数，数据的变化会反馈到模型 store
@@ -65,35 +92,60 @@ var chtPotato={
            *
            * 解决办法是：将其抽象出来编写一个 视图助手 helper
            */
-          store(ADD_KEY, this.todos);
+          store(APP_KEY, this.todos);
     },
-  
-    getItemIndex:function(item) {
-        var itemIndex;
-        if (item.target.tagName.toLowerCase() === 'li') {
-           var arr = Array.prototype.slice.call(this.todoListItem);
-           var index = arr.indexOf(item.target);
-           return itemIndex=index;
-        }
-    },
- 	start:function (argument) {
- 		
- 	},
- 	pause:function() {
+   	start:function (argument) {
+   		
+   	},
+   	pause:function() {
 
- 	},
-    addItem:function(e) {
-         var id = Number(new Date());
-         var text = this.contentBox.value;
-         var addTodo = new Todo(id, text);
-         this.todos.unshift(addTodo); // 模型发生改变
-         this.render(); // 当模型发生改变，触发视图更新
+   	},
+    addItem:function() {
+      //var taskName='';
+      swal({   
+        title: "新建任务",   
+        text: "输入任务名称:",   
+        type: "input",   
+        showCancelButton: true,   
+        closeOnConfirm: false,   
+        animation: "slide-from-top",   
+        inputPlaceholder: "" 
+      },function(inputValue){   
+        if (inputValue === false){
+            return false;
+        } 
+        if (inputValue === "") {
+            swal.showInputError("请输入任务名称!");
+            return false;
+        }      
+        //taskName=inputValue;
+        var id = Number(new Date());
+        var addTodo = new Todo(id, inputValue);
+        this.todos.push(addTodo); // 模型发生改变
+        this.render(); // 当模型发生改变，触发视图更新
+        swal("添加成功", "任务名称: " + inputValue, "success");
+      }.bind(this)
+      );
+      
     },
- 
-    delItem:function(item) {
-         var index = this.getItemIndex(item);
-         this.todos.splice(index, 1);
-         this.render();
+   
+    delItem:function(index,content) {
+      swal({   
+        title: "确定删除?",   
+        text: content,   
+        type: "warning",   
+        showCancelButton: true,   
+        confirmButtonColor: "#DD6B55",   
+        confirmButtonText: "确定",   
+        closeOnConfirm: false 
+      }, 
+      function(){   
+          this.todos.splice(index, 1);
+          this.render();
+          swal(content+"已从列表中删除", "success"); 
+      }.bind(this)
+      );
+     
     }
 
 };
